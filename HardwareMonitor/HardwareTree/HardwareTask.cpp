c7c3e40134fb9cf6ead37b77b6554cb659476f96
Yaroslav <yabaranovst@gmail.com>
2024-09-30 19:15:16 +0700

    Deleted unnecessary files

--- HardwareMonitor/HardwareTree/HardwareTask.cpp
+++
@@ -1,9 +0,0 @@
-#include "HardwareTask.h"
-#include <QThread>
-
-void HardwareTask::updateSensorData()
-{
-   auto rootItem = m_hardwareDataExtractor.getHardwareData();
-   emit sensorDataUpdated(rootItem);
-}
-
--- HardwareMonitorLib/ManageCodeWrappers/UpdateVisitorWrapper.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../ManageCode/UpdateVisitor.h"
-#include <msclr/auto_gcroot.h>
-
-struct UpdateVisitorWrapper
-{
-   msclr::auto_gcroot<UpdateVisitor^> API;
-};
